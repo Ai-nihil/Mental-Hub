45593cfaa4fcf61ee2cf67bf2f961e815cf49afd
Ron <drewrelayo25@gmail.com>
2023-04-22 13:19:20 +0800

    Working Google SSO

--- app/src/main/java/com/example/mentalhub/Login.java
+++ app/src/main/java/com/example/mentalhub/Login.java
@@ -3,8 +3,10 @@ package com.example.mentalhub;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -16,20 +18,34 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.GoogleSignatureVerifier;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
 
     EditText editTextEmail, editTextPassword;
-    Button loginButton;
+    Button loginButton, googleLoginBtn;
     FirebaseAuth mAuth;
+    FirebaseDatabase database;
     ProgressBar progressBar;
     TextView textView;
+    GoogleSignInClient mGoogleSignInClient;
+    ProgressDialog progressDialog;
 
     @Override
     public void onStart() {
@@ -47,56 +63,121 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
         editTextEmail = findViewById(R.id.emailAdd);
         editTextPassword = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.registerNow);
+        googleLoginBtn = findViewById(R.id.googleLogin);
 
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        // Single Sign On Google.
+        progressDialog = new ProgressDialog(Login.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating your account");
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleLoginBtn.setOnClickListener((v) -> {
+            signIn();
+        });
+
+        textView.setOnClickListener((v) -> {
                 Intent intent = new Intent(getApplicationContext(), Register.class);
                 startActivity(intent);
                 finish();
-            }
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        loginButton.setOnClickListener((v) -> {
                 String email, password;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
 
-            if(TextUtils.isEmpty(email)) {
-                editTextEmail.setError("Enter your email");
-            } else if (TextUtils.isEmpty(password)) {
-                editTextPassword.setError("Enter your password");
-            } else {
-                progressBar.setVisibility(View.VISIBLE);
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Toast.makeText(Login.this, "Login Success!.",
-                                            Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                    startActivity(intent);
+                if (TextUtils.isEmpty(email)) {
+                    editTextEmail.setError("Enter your email");
+                } else if (TextUtils.isEmpty(password)) {
+                    editTextPassword.setError("Enter your password");
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(Login.this, "Login Success!.",
+                                                Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
 
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmailAndPassword:failure", task.getException());
-                                    Toast.makeText(Login.this, "Login failed.",
-                                            Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmailAndPassword:failure", task.getException());
+                                        Toast.makeText(Login.this, "Login failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+            }
+        });
+    }
+
+    // Sign in INT
+    int RC_SIGN_IN = 40;
+
+    private void signIn() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
             }
         }
-        });
+    }
+
+    private void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Users users = new Users();
+                            assert user != null;
+                            users.setUserId(user.getUid());
+                            users.setName(user.getDisplayName());
+                            users.setProfile(Objects.requireNonNull(user.getPhotoUrl()).toString());
+
+                            database.getReference().child("Users")
+                                    .child(user.getUid()).setValue(users);
+
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithGoogle:failure", task.getException());
+                            Toast.makeText(Login.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
