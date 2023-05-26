3a34affd185c86f8d0bf1f92f2d0acecfb9528b9
Ron <drewrelayo25@gmail.com>
2023-05-27 00:21:07 +0800

    Fully functional EAT26

--- app/src/main/java/com/example/mentalhub/screening/eat26results.java
+++ app/src/main/java/com/example/mentalhub/screening/eat26results.java
@@ -30,7 +30,7 @@ public class eat26results extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
     String userId;
-    TextView possibleRisk, youAreAtRiskOf;
+    TextView possibleRisk, youAreAtRiskOf, riskDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +41,7 @@ public class eat26results extends AppCompatActivity {
         user = mAuth.getCurrentUser();
         possibleRisk = findViewById(R.id.possibleRisk);
         youAreAtRiskOf = findViewById(R.id.youAreAtRiskOf);
+        riskDescription = findViewById(R.id.riskDescription);
 
         // Gets the user's EAT26 results
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -56,14 +57,46 @@ public class eat26results extends AppCompatActivity {
                     Log.e("firebase", "Error getting data", task.getException());
                 } else {
                     if (Integer.parseInt(String.valueOf(task.getResult().getValue())) < 20) {
-                        possibleRisk.setText("Nothing, you are not at risk of maladaptive eating behavior");
-                    } else if (!task.getResult().exists()){
-                        youAreAtRiskOf.setVisibility(View.INVISIBLE);
-                        possibleRisk.setText("You need to take the EAT26 assessment first");
+                        possibleRisk.setText("Nothing");
+                        riskDescription.setText("you are not at risk of maladaptive eating behavior");
+                    } else {
+                        databaseReference.child("Users").child(userId).child("bulimiaPoints").get().addOnCompleteListener(bulimiaTask -> {
+                            if (bulimiaTask.isSuccessful()) {
+                                int bulimiaScore = Integer.parseInt(String.valueOf(bulimiaTask.getResult().getValue()));
+
+                                databaseReference.child("Users").child(userId).child("dietingPoints").get().addOnCompleteListener(dietingTask -> {
+                                    if (dietingTask.isSuccessful()) {
+                                        int dietingScore = Integer.parseInt(String.valueOf(dietingTask.getResult().getValue()));
+
+                                        databaseReference.child("Users").child(userId).child("oralControlPoints").get().addOnCompleteListener(oralControlTask -> {
+                                            if (oralControlTask.isSuccessful()) {
+                                                int oralControlScore = Integer.parseInt(String.valueOf(oralControlTask.getResult().getValue()));
+
+                                                if (dietingScore >= bulimiaScore && dietingScore >= oralControlScore) {
+                                                    possibleRisk.setText("Bulimia and food preoccupation (EAT-BUL)");
+                                                    riskDescription.setText("The preoccupation with thoughts about food and attempts to vomit food eaten during a binge");
+                                                } else if (bulimiaScore >= oralControlScore) {
+                                                    possibleRisk.setText("Dietary (EAT-DIET)");
+                                                    riskDescription.setText("The preoccupation with being thinner and avoidance of fattening foods");
+                                                } else {
+                                                    possibleRisk.setText("Oral Control (EAT-ORAL)");
+                                                    riskDescription.setText("Attempts to maintain self-control while eating and the perceived pressure from others to gain weight");
+                                                }
+                                            } else {
+                                                // Handle the error for oralControlPoints task
+                                            }
+                                        });
+                                    } else {
+                                        // Handle the error for dietingPoints task
+                                    }
+                                });
+                            } else {
+                                // Handle the error for bulimiaPoints task
+                            }
+                        });
                     }
                 }
             }
         });
     }
-
 }
