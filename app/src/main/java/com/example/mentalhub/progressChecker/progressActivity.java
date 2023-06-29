725c1aad94d8cecca45bd685df4e72958cb333aa
Ron <drewrelayo25@gmail.com>
2023-06-30 01:27:44 +0800

    Fixed crash on progress checking

--- app/src/main/java/com/example/mentalhub/progressChecker/progressActivity.java
+++ app/src/main/java/com/example/mentalhub/progressChecker/progressActivity.java
@@ -1,9 +1,9 @@
 package com.example.mentalhub.progressChecker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mentalhub.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,7 +21,7 @@ public class progressActivity extends AppCompatActivity {
 
     TextView points;
 
-    int quizPoints, breathingPoints, mindPoints, cognitivePoints, journalPoints, problemSolvingPoints;
+    int quizPoints = 0, breathingPoints = 0, mindPoints = 0, cognitivePoints = 0, journalPoints = 0, problemSolvingPoints = 0;
 
     int maxPoints = 10000;
     int previousPoints = 0;
@@ -49,44 +49,49 @@ public class progressActivity extends AppCompatActivity {
                 if (recordedQuizScore.getResult().getValue() == null) {
                     // Child "quizPoints" does not exist, create it with the initial value of quizPoints
                     databaseReference.child("Users").child(user.getUid()).child("quizPoints").setValue(quizPoints);
+                } else {
+                    quizPoints = Integer.parseInt(String.valueOf(recordedQuizScore.getResult().getValue()));
                 }
-                quizPoints = Integer.parseInt(String.valueOf(recordedQuizScore.getResult().getValue()));
                 databaseReference.child("Users").child(user.getUid()).child("breathingPoints").get().addOnCompleteListener(recordedBreathingScore -> {
                     if (recordedBreathingScore.isSuccessful()) {
                         if (recordedBreathingScore.getResult().getValue() == null) {
                             // Child "breathingPoints" does not exist, create it with the initial value of quizPoints
                             databaseReference.child("Users").child(user.getUid()).child("breathingPoints").setValue(breathingPoints);
+                        } else {
+                            breathingPoints = Integer.parseInt(String.valueOf(recordedBreathingScore.getResult().getValue()));
                         }
-                        breathingPoints = Integer.parseInt(String.valueOf(recordedBreathingScore.getResult().getValue()));
                         databaseReference.child("Users").child(user.getUid()).child("mindPoints").get().addOnCompleteListener(recordedMindScore -> {
                             if (recordedMindScore.isSuccessful()) {
                                 if (recordedMindScore.getResult().getValue() == null) {
                                     // Child "mindPoints" does not exist, create it with the initial value of quizPoints
                                     databaseReference.child("Users").child(user.getUid()).child("mindPoints").setValue(mindPoints);
+                                } else {
+                                    mindPoints = Integer.parseInt(String.valueOf(recordedMindScore.getResult().getValue()));
                                 }
-                                mindPoints = Integer.parseInt(String.valueOf(recordedMindScore.getResult().getValue()));
                                 databaseReference.child("Users").child(user.getUid()).child("problemSolvingPoints").get().addOnCompleteListener(recordedProblemSolvingScore -> {
                                     if (recordedProblemSolvingScore.isSuccessful()) {
                                         if (recordedProblemSolvingScore.getResult().getValue() == null) {
                                             // Child "problemSolvingPoints" does not exist, create it with the initial value of quizPoints
                                             databaseReference.child("Users").child(user.getUid()).child("problemSolvingPoints").setValue(problemSolvingPoints);
+                                        } else {
+                                            problemSolvingPoints = Integer.parseInt(String.valueOf(recordedProblemSolvingScore.getResult().getValue()));
                                         }
-                                        problemSolvingPoints = Integer.parseInt(String.valueOf(recordedProblemSolvingScore.getResult().getValue()));
                                         databaseReference.child("Users").child(user.getUid()).child("journalPoints").get().addOnCompleteListener(recordedJournalScore -> {
                                             if (recordedJournalScore.isSuccessful()) {
                                                 if (recordedJournalScore.getResult().getValue() == null) {
                                                     // Child "problemSolvingPoints" does not exist, create it with the initial value of quizPoints
                                                     databaseReference.child("Users").child(user.getUid()).child("journalPoints").setValue(journalPoints);
+                                                } else {
+                                                    journalPoints = Integer.parseInt(String.valueOf(recordedJournalScore.getResult().getValue()));
                                                 }
-                                                journalPoints = Integer.parseInt(String.valueOf(recordedJournalScore.getResult().getValue()));
                                                 databaseReference.child("Users").child(user.getUid()).child("cognitivePoints").get().addOnCompleteListener(recordedCognitiveScore -> {
                                                     if (recordedCognitiveScore.isSuccessful()) {
                                                         if (recordedCognitiveScore.getResult().getValue() == null) {
                                                             // Child "cognitivePoints" does not exist, create it with the initial value of quizPoints
                                                             databaseReference.child("Users").child(user.getUid()).child("cognitivePoints").setValue(cognitivePoints);
+                                                        } else {
+                                                            cognitivePoints = Integer.parseInt(String.valueOf(recordedCognitiveScore.getResult().getValue()));
                                                         }
-                                                        cognitivePoints = Integer.parseInt(String.valueOf(recordedCognitiveScore.getResult().getValue()));
-
                                                         // Calculate the combinedPoints inside this last callback
                                                         combinedPoints = quizPoints + mindPoints + breathingPoints + cognitivePoints + problemSolvingPoints + journalPoints;
                                                         // Sets progress bar to specific amount of progress
